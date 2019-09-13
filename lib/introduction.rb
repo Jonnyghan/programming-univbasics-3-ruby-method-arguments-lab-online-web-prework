0c084e3a6e8bec3cbf04cd74ce829353d8a25321
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-09-13 17:50:21 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,7 +12,7 @@ def introduction_with_language(name = "Dan", language = "Ember.js")
 end
 
 def introduction_with_language_optional(name = "Edwin", language = "Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
